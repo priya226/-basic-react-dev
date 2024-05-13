4cd478b25cb206c295882ae70371021e30b3d05c
priya226 <pr35@iitbbs.ac.in>
2024-05-13 10:20:09 +0530

    shimmer Ui config for loading purpose

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,11 @@
 import {restaurantsList} from "../utils/mocData" //named export import like this
 import { useEffect, useState } from "react";
 import RestaurantCard from "./RestarauntCard" // restaurantCard is defsult export hence it should not be called under {}, it gives error
+import Shimmer from  "./Shimmer";
 const Body= () =>{
     // https://dev.to/shiv1998/why-not-to-use-index-as-key-in-react-lists-practical-example-3e66
    //Local State Variable - Super poerful variable
-    const [restaurantsListData, setListOfRestaraunt] =  useState(restaurantsList) // first arg is variable and second is fn to update the variable
+    // const [restaurantsListData, setListOfRestaraunt] =  useState(restaurantsList) // first arg is variable and second is fn to update the variable
     // we just pass the data as arg into fn and it will internally work as assigning new value to variable
     // we can use this fn whenever we want to update
     //let  restaurantsList = [] NOrmal JS Variable assignment
@@ -13,6 +14,8 @@ const Body= () =>{
     // const restaurantsListData = arr[0]
     // const setListOfRestaraunt = arr[1]
 
+
+    const [restaurantsListData, setListOfRestaraunt] =  useState([]) // first arg is variable and second is fn to update the variable
 
 
     //wehener the state variable updates of rerender the element
@@ -54,6 +57,11 @@ const Body= () =>{
       console.error('fetched error',error)
     }
    };
+
+   if(restaurantsListData.length===0){
+    console.log('shimmer called')
+    return <Shimmer/>
+   }
 
    console.log('body render')
     return (
---
+++ src/components/Shimmer.js
@@ -0,0 +1,10 @@
+const Shimmer =() =>{
+    console.log('shimmer cards')
+    return (<div className="shimmer-container">
+          <div className="res-card"></div>
+          <div className="res-card"></div>
+         <div className="res-card"></div>
+         <div className="res-card"></div>
+          </div>)
+}
+export default Shimmer;
