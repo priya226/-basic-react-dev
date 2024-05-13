96838c5f1ce2e787315996ecdeb66e683e5e3ae9
priya226 <pr35@iitbbs.ac.in>
2024-05-13 16:27:37 +0530

    login state variable change practice

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import { IMG_CDN_URL } from "../utils/constant";
 export const Header =()=>{
+   const [loginStatus,setUserLogin]=useState('login')
     return (
         <div className='header'>
             <div className='logo-container'>
@@ -11,6 +13,13 @@ export const Header =()=>{
                     <li>About Us</li>
                     <li>Contact Us</li>
                     <li>Cart</li>
+                    {/* conditional rendering of element */}
+                    <button onClick={()=>{
+                        loginStatus==='login'?setUserLogin('logout'):setUserLogin('login')
+                    }
+                    }>
+                        {loginStatus}
+                    </button>
                 </ul>
             </div>
         </div>
