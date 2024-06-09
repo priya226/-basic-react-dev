74c201388dd32fea03b60c5c93f0f8b098171117
priya226 <pr35@iitbbs.ac.in>
2024-06-10 01:30:36 +0530

    router error hanlder

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,6 +1,17 @@
-const Error = ()=>{
-    return <div>
+import { useRouteError ,Link} from "react-router-dom"; // import useRouteError for routing error
+import errorImage from "../../Assest/errorImg.avif";
 
+const Error = ()=>{
+      // call useRouteError so we can access error data while routing
+
+    const err = useRouteError()
+    return <div className="error-page">
+        <img  src={errorImage} />
+        <h1>Oops! The restaurant you're looking for can't be found.</h1>
+      <h3 className="error-data">{err.data}</h3>
+      <h3 className="error-back-home">
+        <Link to="/">Back Home</Link>
+      </h3>
     </div>
 }
 export default Error;
