05e68724979f6f9104437c324eec25b8f6fa7464
priya226 <pr35@iitbbs.ac.in>
2024-09-16 16:51:12 +0530

    using immer for immutability

--- redux/src/nested-state.js
+++ redux/src/nested-state.js
@@ -1,5 +1,5 @@
 const redux = require('redux');
-
+const produce = require('immer').produce;
 const initialState ={
     name :"Priya",
     address:{
@@ -18,14 +18,26 @@ const updateStreet =(street) =>{
     }
 }
 
+/**
+ * 
+In Redux, reducers must return a new state object whenever the state is updated. Mutating the existing state directly would break Redux’s core principles.
+ */
+/**
+ * Drafts for Mutative Logic: With Immer, you can write code that "mutates" state in a way that feels natural, but Immer handles the immutability for you. It does this by creating a draft of the state, tracking changes to the draft, and then generating a new immutable state from the modified draft.
+ */
 const reducer =(state=initialState,action)=>{
     switch(action.type){
         case STREET_UPDATE:
-            return {...state,
-                address:{
-                    ...state.address,
-                    street:action.payload
-                }}
+            // return {...state,
+            //     address:{
+            //         ...state.address,
+            //         street:action.payload
+            //     }}
+            return produce(state,(draft)=>{ //this fn takes base state,draft (proxy of base state)
+                //where we can apply direct change
+                // Once the producer function has finished making changes to the draft, Immer produces a new immutable state by applying those changes to the draft.
+                draft.address.street = action.payload
+            })
         default:
             return state
     }
