a53e136bdcd8a2dd046afad7f5d648b0365b5da0
priya226 <pr35@iitbbs.ac.in>
2024-09-16 15:02:56 +0530

    create seperate reducer handler for icecream and combine boioith reducers into one and pass it to store

--- redux/src/App.js
+++ redux/src/App.js
@@ -2,7 +2,8 @@ const { act } = require('react');
 const redux = require('redux');
 // import {redux} from 'redux';
 const createStore = redux.createStore  //get createStore functionality step 1
-const bindActionCreator = redux.bindActionCreators;
+const bindActionCreator = redux.bindActionCreators; // to bind our actions function into reduce.dispatch
+const combineReducers =redux.combineReducers; // to combine our multiple reducers into single so that createstore we can pass only combined reducer
 
 //create function to return object with action type step 2
 const CAKE_ORDERED = 'CAKE_ORDERED'
@@ -14,6 +15,10 @@ const ICECREAM_RESTOCKED='ICECREAM_RESTOCKED'
 //initialise object step 2
 const initialCakestate ={ //application state should be represented by single object
     numofcakes:10,
+    // numOfIcecream:10
+}
+
+const initialIcecreamstate ={ //creating seperate reducer to handle icecream functionality
     numOfIcecream:10
 }
 
@@ -63,10 +68,31 @@ const cakeReducer =(state = initialCakestate,action)=>{
     }
 }
 
+// reducer function return state based on action type
+const iceCreamReducer =(state = initialIcecreamstate,action)=>{
+    switch(action.type){
+        case ICECREAM_ORDERED:
+             return {...state,numOfIcecream:state.numOfIcecream-action.payload}
+        case ICECREAM_RESTOCKED:
+             return {...state,numOfIcecream:state.numOfIcecream+action.payload}
+        default:
+             return state;
+    }
+}
+
+/** 
 // create store by passing reducer whichh has all functionality define
 //  step3
 //store for cakereducer 
-const store = createStore(cakeReducer)
+const store = createStore(cakeReducer) */
+
+// craeete combinereducer and make use it in store
+const rootReducer=combineReducers({
+    cake:cakeReducer,
+    icecream:iceCreamReducer
+})
+
+const store = createStore(rootReducer)
 
 console.log('initial state',store.getState());
 
