0ff1cf23856ed162ad1c43be09b01179c1d33f76
priya226 <pr35@iitbbs.ac.in>
2024-09-16 13:16:23 +0530

    added restock action also in reducer and used restock by using store

--- redux/src/App.js
+++ redux/src/App.js
@@ -4,29 +4,45 @@ const createStore = redux.createStore  //get createStore functionality step 1
 
 //create function to return object with action type step 2
 const CAKE_ORDERED = 'CAKE_ORDERED'
-function orderCake(qt=1){ //action creator return object has action
-    return {
-        type:CAKE_ORDERED, // action a object with type of action
-        quantity:qt
-    }
-}
+const CAKE_RESTOCKED='CAKE_RESTOCKED'
+
 
 //initialise object step 2
 const initialCakestate ={ //application state should be represented by single object
     numofcakes:10,
 }
 
+// functionality for ordercake
+function orderCake(qt=1){ //action creator return object has action
+    return {
+        type:CAKE_ORDERED, // action a object with type of action
+        payload:qt
+    }
+}
+
+//functionality for restock cake
+function restockCake(qt=1){
+    return {
+        type: CAKE_RESTOCKED,
+        payload:qt
+    }
+}
+
 // reducer function return state based on action type
 const cakeReducer =(state = initialCakestate,action)=>{
     switch(action.type){
         case CAKE_ORDERED:
-            return { ...state, numofcakes : state.numofcakes-1} //other properties should be same hence spread operator
+            return { ...state, numofcakes : state.numofcakes-action.payload} //other properties should be same hence spread operator
+        case CAKE_RESTOCKED:
+            return {...state,numofcakes:state.numofcakes+action.payload}
         default:
              return state;
     }
 }
 
-// create store by passing reducer function step3
+// create store by passing reducer whichh has all functionality define
+//  step3
+//store for cakereducer 
 const store = createStore(cakeReducer)
 
 console.log('initial state',store.getState());
@@ -36,7 +52,7 @@ const unsubscribe =store.subscribe(()=>{console.log('updated state ',store.getSt
 store.dispatch(orderCake())
 store.dispatch(orderCake())
 store.dispatch(orderCake())
-
+store.dispatch(restockCake(3))
 //need to unsubscribe the data after use 
 unsubscribe();
 // (prevstate,action)=>newstate
