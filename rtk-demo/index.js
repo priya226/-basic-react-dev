713dc978efb3ab4712471031e5bcf01bfe904652
priya226 <pr35@iitbbs.ac.in>
2024-09-26 22:40:47 +0530

    extrareducer handing

--- rtk-demo/index.js
+++ rtk-demo/index.js
@@ -9,12 +9,12 @@ console.log('initial state..',store.getState());
 const unsubscribe = store.subscribe(()=>{
     console.log('updated state..',store.getState())
 });
-// store.dispatch(cakeAction.ordered())
-// store.dispatch(cakeAction.ordered())
-// store.dispatch(cakeAction.ordered())
-// store.dispatch(cakeAction.restocked(4))
+store.dispatch(cakeAction.ordered()) //action type willbe cake/ordered
+store.dispatch(cakeAction.ordered())
+store.dispatch(cakeAction.ordered())
+store.dispatch(cakeAction.restocked(4))
 
-store.dispatch(icecreamAction.ordered())
+store.dispatch(icecreamAction.ordered())//action type will be icecream/ordered
 store.dispatch(icecreamAction.ordered())
 store.dispatch(icecreamAction.ordered())
 store.dispatch(icecreamAction.restocked(4))
